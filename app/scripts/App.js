613b1dfd55c4cba26b866d51345ed65029036a50
Cody Stebbins <cody@stebbins.io>
2013-08-27 17:13:55 -0700

    generate initial yeoman project add pico modal library

---
+++ app/scripts/App.js
@@ -0,0 +1,10 @@
+/*global define */
+define([
+  "Configuration/MainConfiguration.js",
+  "Libraries/PicoModal.js"
+], function () {
+    'use strict';
+
+    picoModal("Ah, the pitter patter of tiny feet in huge combat boots.");
+    return '\'Allo \'Allo!';
+});
