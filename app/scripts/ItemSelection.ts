4a3912b67f63cf9192fcab45b64ae0f2151042be
Cody Stebbins <cody@trov.com>
2013-09-24 01:16:15 -0700

    allow folders to be clicked on create unique classes as params for styling.

--- app/scripts/ItemSelection.ts
+++ app/scripts/ItemSelection.ts
@@ -1,31 +1,71 @@
 ///<reference path="../bower_components/types/jquery/jquery.d.ts" />
 
-export class ItemSelection {
+module ItemMirror {
 
-  private nav: any;
-  private list: any;
+  export class ItemSelection {
 
-  constructor(private itemMirror,
-    private id: string) {
+    private nav: JQuery;
+    private list: JQuery;
+    private listPath: JQuery;
+    private static itemMirrorGUID: string = "data-item-mirror-guid";
 
-    this.nav = $(document.getElementById(id));
-    this.itemMirror.listAssociations((error, GUIDs) => {
-      this.list = $('<ul class="nav nav-list _itemMirrorSelectionList"></ul>');
+    // TODO: type of itemMirror
+    constructor(
+      private itemMirror: Object,
+      private id: string,
+      private listClass: string = "itemMirrorList",
+      private listPathClass: string = "itemMirrorListPath",
+      private listItemClass: string = "itemMirrorListItem,") {
+
+      this.nav = $("#" + id);
+      this.list = $("<ul class='nav nav-list" + this.listClass +"'></ul>");
+      this.listPath = $("<p class='nav-header " + this.listPathClass + "'></p>");
+
       this.nav.append(this.list);
-      this.list.append('<li class="nav-header _itemMirrorSelectionPath"></li>')
+      this.nav.append(this.listPath);
+      this.loadItemMirror();
+    }
 
-      this.itemMirror.getGroupingItemURI((error, groupingItemURI) => {
-        this.nav.find("._itemMirrorSelectionPath").text("Current Path: " + groupingItemURI);
-      });
-
-      //TODO: Implement folder selection
-      for (var i = 0; i < GUIDs.length; i += 1) {
-        var GUID = GUIDs[i];
-
-        this.itemMirror.getAssociationDisplayText(GUID, (error, displayName) => {
-          this.list.append('<li><a href="#">' + displayName + '</a></li>');
-        });
+    private loadItemMirror(GUID?: string): void {
+      if (GUID) {
+       this.itemMirror.createItemMirrorForAssociatedGroupingItem(GUID, (error, itemMirror) => {
+          this.createListFromAssociations(itemMirror);
+        })
+      } else {
+        this.createListFromAssociations(this.itemMirror);
       }
-    });
+    }
+
+    private createListFromAssociations(itemMirror): void {
+      var self: ItemSelection = this;
+
+      itemMirror.listAssociations((error, GUIDs) => {
+        itemMirror.getGroupingItemURI((error, groupingItemURI) => {
+          this.listPath.text("Path: " + groupingItemURI);
+        });
+
+        var listItemClick = function () { self.listItemClick($(this)) };
+        for (var i = 0; i < GUIDs.length; i += 1) {
+          var GUID = GUIDs[i];
+
+          itemMirror.getAssociationDisplayText(GUID, (error, displayName) => {
+            var listItem = $("<li class='" + this.listItemClass + "'><a href='#'>" + displayName + "</a></li>");
+            listItem.click(listItemClick);
+            listItem.attr(ItemSelection.itemMirrorGUID, GUID);
+            this.list.append(listItem);
+          });
+        }
+      });
+    }
+
+    private listItemClick(element: JQuery): void {
+      var GUID = element.attr(ItemSelection.itemMirrorGUID);
+      this.list.children().remove();
+      this.loadItemMirror(GUID);
+    }
+
   }
+
 }
+
+
